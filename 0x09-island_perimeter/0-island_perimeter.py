70703a1119c2b12d517b533ad35d95a821a08ad1
lyndha <ihuomalyndha@gmail.com>
2024-05-10 00:21:58 +0100

    grid perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,39 +1,23 @@
-
 #!/usr/bin/python3
 """
-0-island_perimeter.py file
+    function def island_perimeter(grid): that returns
+    the perimeter of the island described in grid:
 """
 
 
 def island_perimeter(grid):
-    """
-    it get the number of rows in the grid
-    returns the perimeter of the island described in grid
-    """
-    rows = len(grid)
-    if rows == 0:
-        return 0
-
-    # get the number of columns in the grid
-    columns = len(grid[0])
+    """return the perimeter of the island described in grid"""
     perimeter = 0
-
-    # iterate over each cell in the grid
-    for row in range(rows):
-        for col in range(columns):
-            if grid[row][col] == 1:
-                # check top
-                if row == 0 or grid[row - 1][col] == 0:
-                    perimeter += 1
-                # check bottom
-                if row == rows - 1 or grid[row + 1][col] == 0:
-                    perimeter += 1
-                # check left
-                if col == 0 or grid[row][col - 1] == 0:
-                    perimeter += 1
-                # check right
-                if col == columns - 1 or grid[row][col + 1] == 0:
-                    perimeter += 1
-
-    # return the calculated perimeter
+    i = 0
+    while i < len(grid):
+        j = 0
+        while j < len(grid[i]):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i - 1 >= 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j - 1 >= 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+            j += 1
+        i += 1
     return perimeter
